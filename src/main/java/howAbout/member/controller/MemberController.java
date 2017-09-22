8f4a9da5f1ccc5e27f4a431991a8a085e7a3c43f
Michael <maengbk84@gmail.com>
2017-09-22 20:17:44 +0900

    member추가

--- src/main/java/howAbout/member/controller/MemberController.java
+++ src/main/java/howAbout/member/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class memberController {
+public class MemberController {
 
 	@RequestMapping("main")
 	public String main() {
---
+++ src/main/java/howAbout/member/models/Grade.java
@@ -0,0 +1,11 @@
+package howAbout.member.models;
+
+public class Grade {
+	private String	grade_id;
+	private String	grade_name;
+	private int	grade_exp;
+	private int	grade_like;
+	private int	grade_write;
+	private int	grade_reply;
+
+}
